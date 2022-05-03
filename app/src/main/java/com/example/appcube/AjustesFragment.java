74ff677968d8e2535823cba6ef8edc12ea0451d9
pablogonzaleztaberna <tabernagonzalezpablo@gmail.com>
2022-05-03 11:56:31 +0200

    Diseño Pantalla Ajustes

--- app/src/main/java/com/example/appcube/AjustesFragment.java
+++ app/src/main/java/com/example/appcube/AjustesFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,4 +17,6 @@ public class AjustesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_ajustes, container, false);
     }
+
+
 }
